4215d146d7a5eb60d7ede5e1a608f888f6501925
ilterates <iltates@gmail.com>
2016-03-26 14:32:28 -0700

    created divs and mouseover events

---
+++ js/js.js
@@ -0,0 +1,14 @@
+console.log("Js works");
+
+$(document).ready(function () {
+
+
+    $(".off").mouseover( function (name) {
+      console.log("OFF");
+    });
+
+    $("#start").mouseover(function(){
+      console.log("started");
+
+    });
+});
