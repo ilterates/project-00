0824cb7db8d7ebd0f5f56d49e21f1fc4cf1f7436
ilterates <iltates@gmail.com>
2016-03-26 18:20:25 -0700

    stop watch semi working. timer starts on mouseover at start and finishes on mouseover finish. if player fails, timer stops

--- js/js.js
+++ js/js.js
@@ -1,14 +1,88 @@
-console.log("Js works");
+// object
+var timer = new stopwatch();
+var gameOn = true;
+// stopwatch
 
-$(document).ready(function () {
+function stopwatch() {
+
+var time = 0;
+var interval;
+var offset;
+
+function update(){
+  time += delta();
+//  var formattedTime = timeFormatter(time);
+  console.log(time);
+}
+function delta(){
+  var now = Date.now();
+  var timePassed = now - offset;
+  offset = now;
+  return timePassed;
+}
+function timeFormatter(timeInMilliseconds){
+  var time = new Date(timeInMilliseconds);
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+  var milliseconds = time.getMilliseconds();
+}
+
+this.isOn = false;
 
 
-    $(".off").mouseover( function (name) {
-      console.log("OFF");
+this.start = function() {
+// aka if (this.isOn === false)
+  if (!this.isOn) {
+    interval = setInterval(update, 10);
+    offset = Date.now();
+    this.isOn = true;
+  }
+
+};
+
+  this.stop = function () {
+    if (this.isOn){
+      clearInterval(interval);
+      interval = null;
+      this.isOn = false;
+    }
+  };
+
+  this.reset = function () {
+    time = 0;
+  };
+
+  // on.mouseover
+  $("#finish").mouseover( function (name) {
+    console.log("Finished", time);
+    if (timer.isOn) {
+      timer.stop();
+    }
+  });
+
+  $("#start").mouseover(function(){
+    gameOn = true;
+    $(".container").fadeTo( "fast" , 1, function() {
+
     });
+    console.log("started");
+    if (!timer.isOn) {
+      timer.reset();
+      timer.start();
+    }
+  });
+  $(".off").mouseover(function(){
+    if (gameOn) {
+      $(".container").fadeTo( "fast" , 0.25, function(off) {
 
-    $("#start").mouseover(function(){
-      console.log("started");
+      });
+    }
+    if (gameOn) {
+      $(".container").effect( "shake",{times:2}, 270  );
+      console.log("failed", time);
+      timer.stop();
+      gameOn = false;
+    }
 
-    });
-});
+  });
+}
